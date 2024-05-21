601b0f71964303bc1e44142488469caf0c725fa5
Tosin5S <olyguy.aq@gmail.com>
2024-05-21 07:33:57 +0100

    update random_forest_regressor.py and views.py

--- backend/app/views.py
+++ backend/app/views.py
@@ -1,21 +1,25 @@
+import pandas as pd
 from django.http import JsonResponse
 from .models import YieldPrediction
-from sklearn.ensemble import RandomForestRegressor
+from .random_forest_regressor import RandomForestRegressorWrapper
 from django.views.decorators.csrf import csrf_exempt
 
 @csrf_exempt
 def predict_yield(request):
     if request.method == 'POST':
-        germplasmDbId = request.POST.get('germplasmDbId')
-        cassava_mosaic_disease_severity = request.POST.get('cassava_mosaic_disease_severity')
         
-        # Create a new instance of the YieldPrediction model
-        new_df = YieldPrediction(germplasmDbId=germplasmDbId, cassava_mosaic_disease_severity=cassava_mosaic_disease_severity)
+        cassava_mosaic_disease_severity = request.POST.get('cassava mosaic disease severity 6-month evaluation|CO_334:0000194')
+        
+        # Create a new instance of the RandomForestRegressorWrapper
+        rf_wrapper = RandomForestRegressorWrapper()
+        X = rf_wrapper.get_X()
+        new_df = pd.DataFrame(columns=X.columns)
+        new_df.loc['cassava mosaic disease severity 6-month evaluation|CO_334:0000194'] = cassava_mosaic_disease_severity
         
         # Make predictions
-        predictions = RandomForestRegressor().predict(new_df)
+        predictions = rf_wrapper.predict(new_df)
         
         # Return the predictions as JSON
-        return JsonResponse({'yield_prediction': predictions[0]})
+        return JsonResponse({'fresh root yield|CO_334:0000013': predictions[0]})
     else:
         return JsonResponse({'error': 'Invalid request'})
